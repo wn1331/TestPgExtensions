9c3a97b5588466edefd7b1af310d801bc9707694
wn1331 <wn1331@gmail.com>
2023-06-07 15:25:55 +0900

    쓸모없는 import 제거

--- src/main/java/com/example/testpgextensions/TestPgExtensionsApplication.java
+++ src/main/java/com/example/testpgextensions/TestPgExtensionsApplication.java
@@ -1,7 +1,5 @@
 package com.example.testpgextensions;
 
-import com.github.javafaker.Faker;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
